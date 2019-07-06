2c578d6b4ee651356058fb0b60f56916f05ea876
icepants <shawnluther99@gmail.com>
2019-07-06 15:02:08 -0400

    Reworked many aspects of the CSS media queries, added a portfolio section (Still some work to do there), and a general code cleanup.

--- assets/js/app.js
+++ assets/js/app.js
@@ -113,6 +113,44 @@ $("#linkDiveDeeper").click(function () {
     }, 500);
 });
 
+// Smooth Scroll to 'My Work'.
+$(".my-work").click(function () {
+    // Make the scroll wait a second so the IDS needed to load are present at the time of the scroll request.
+    setTimeout(100);
+    // Show 'about me' image.
+    $("#deepDiveBackground").show();
+    // Show 'about me' content.
+    $(".second").show();
+    // Show 'My Work' Content.
+    $(".third").show();
+    // Animate a smooth scroll.
+    $('html,body').animate({
+        scrollTop: $(".third").offset().top
+    }, 'slow'); // Do it slowly.
+
+    // Base Development Technologies (Progress Animation Transition).
+    $(".html").animate({
+        width: "100%"
+    }, 500);
+    $(".css").animate({
+        width: "100%"
+    }, 500);
+    $(".js").animate({
+        width: "85%"
+    }, 500);
+
+    // Base Tools, Frameworks and Libraries (Progress Animation Transition).
+    $(".vsCode").animate({
+        width: "90%"
+    }, 500);
+    $(".bsFramework").animate({
+        width: "85%"
+    }, 500);
+    $(".jQuery").animate({
+        width: "50%"
+    }, 500);
+});
+
 // Back to 'Home' Section.
 $(".home").click(function () {
     // Make the scroll wait a second so the IDS needed to load are present at the time of the scroll request.
@@ -122,3 +160,8 @@ $(".home").click(function () {
         scrollTop: $(".first").offset().top
     }, 'slow'); // Do it slowly.
 });
+
+// Prevent Project Slider From Auto Scrolling.
+$('.carousel').carousel({
+    interval: false
+}); 
