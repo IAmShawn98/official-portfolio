47577b0dc8d407ba90a0a3a8eadb899a0e1ab8bd
icepants <shawnluther99@gmail.com>
2019-08-21 12:29:02 -0400

    Performed a code cleanup, rewrote the Javascript 'smooth scroll' behavior so it's more easily read, fixed a glitch caused by clicking any scroll button which would make the progress bars flicker and re-perform their initial progress fill behavior when they shouldn't.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,176 +1,139 @@
-// Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyDk9DgoCWaoS_OiVmFzQio8WFmNTq-Q-IM",
-    authDomain: "offical-portfolio.firebaseapp.com",
-    databaseURL: "https://offical-portfolio.firebaseio.com",
-    projectId: "offical-portfolio",
-    storageBucket: "",
-    messagingSenderId: "1077306770175",
-    appId: "1:1077306770175:web:18d350384b2ec81b"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+$(document).ready(function () {
+    // Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyDk9DgoCWaoS_OiVmFzQio8WFmNTq-Q-IM",
+        authDomain: "offical-portfolio.firebaseapp.com",
+        databaseURL: "https://offical-portfolio.firebaseio.com",
+        projectId: "offical-portfolio",
+        storageBucket: "",
+        messagingSenderId: "1077306770175",
+        appId: "1:1077306770175:web:18d350384b2ec81b"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-// Reference to the root of our database.
-var database = firebase.database();
+    // Reference to the root of our database.
+    var database = firebase.database();
 
-// All connections are stored in this location.
-var connectionsRef = database.ref("/connections");
+    // All connections are stored in this location.
+    var connectionsRef = database.ref("/connections");
 
-// Each time a new connection is made, update '/connections'.
-var connectedRef = database.ref(".info/connected");
+    // Each time a new connection is made, update '/connections'.
+    var connectedRef = database.ref(".info/connected");
 
-// When the connection state changes....
-connectedRef.on("value", function (snap) {
+    // When the connection state changes....
+    connectedRef.on("value", function (snap) {
 
-    // If they are connected....
-    if (snap.val()) {
+        // If they are connected....
+        if (snap.val()) {
 
-        // Add user to the connections list.
-        var con = connectionsRef.push(true);
-        // Otherwise, remove them if they disconnect.
-        con.onDisconnect().remove();
-    }
+            // Add user to the connections list.
+            var con = connectionsRef.push(true);
+            // Otherwise, remove them if they disconnect.
+            con.onDisconnect().remove();
+        }
+    });
+
+    // When the page is loaded or when a connection changes....
+    connectionsRef.on("value", function (snap) {
+        // Populate the user count to the DOM.
+        $(".users").text(snap.numChildren());
+    });
+
+
+    // Smooth Scroll to ---> 'About Me'.
+    $('.linkDiveDeeper').on('click', function (event) {
+        // Prevent Reloading.
+        event.preventDefault()
+
+        // Load the '.second' and '.third' classes
+        // so that we can scroll to them.
+        $(".second").show();
+        $(".third").show();
+
+        // Smooth Scroll Animate to the 'second' class.
+        $('html,body').animate({
+            scrollTop: $(".second").offset().top
+        }, 'slow'); // Do it slowly.
+
+        // Base Development Technologies (Progress Bar Animation Transition).
+
+        // HTML Bar Progress.
+        $(".html").animate({
+            width: "100%"
+        }, 500);
+        // CSS Bar Progress.
+        $(".css").animate({
+            width: "100%"
+        }, 500);
+        // JS Bar Progress.
+        $(".js").animate({
+            width: "85%"
+        }, 500);
+
+        // Base Tools, Frameworks and Libraries (Progress Bar Animation Transition).
+
+        // VS Code Bar Progress.
+        $(".vsCode").animate({
+            width: "90%"
+        }, 500);
+        // Bootstrap Bar Progress.
+        $(".bsFramework").animate({
+            width: "85%"
+        }, 500);
+        // jQuery Bar Progress.
+        $(".jQuery").animate({
+            width: "50%"
+        }, 500);
+    });
+
+    // Smooth Scroll to ---> 'My Work'.
+    $('.my-work').on('click', function (event) {
+        // Prevent Reloading.
+        event.preventDefault()
+
+        // Load the '.second' and '.third' classes
+        // so that we can scroll to them.
+        $(".second").show();
+        $(".third").show();
+
+
+        // Smooth Scroll Animate to the 'third' class.
+        $('html,body').animate({
+            scrollTop: $(".third").offset().top
+        }, 'slow'); // Do it slowly.
+    });
+
+    // Smooth Scroll to ---> 'Back to the Top ^'.
+    $('.home').on('click', function (event) {
+        // Prevent Reloading.
+        event.preventDefault()
+
+        // To prevent jerkyness, wait a few seconds, then 
+        // deload the second and third section classes.
+        setTimeout(() => {
+            // Deload the '.second' and '.third' class.
+            // when scrolling back to the top section.
+            $(".second").hide();
+            $(".third").hide();
+        }, 1000);
+
+        // We're scroll to 'circle-brand' because that is a class
+        // of the navbar, which will trigger our jQuery
+        // scroll behavior to go back to the top.
+        $('html,body').animate({
+            scrollTop: $("#circle-brand").offset().top
+        }, 'slow'); // Do it slowly.
+    });
+
+    // Prevent Project Slider From Auto Scrolling.
+    $('.carousel').carousel({
+        interval: false
+    });
+
+    // Get the Current Year.
+    var currentYear = moment().format('YYYY');
+    // Append Current Year to Footer.
+    $("#dynamicYear").text(currentYear);
+
 });
-
-// When the page is loaded or when a connection changes....
-connectionsRef.on("value", function (snap) {
-    // Populate the user count to the DOM.
-    $(".users").text(snap.numChildren());
-});
-
-
-// Smooth Scroll to 'About Me'.
-$("#btnFirstDive").click(function () {
-    // Make the scroll wait a second so the IDS needed to load are present at the time of the scroll request.
-    setTimeout(100);
-    // Show 'about me' image.
-    $("#deepDiveBackground").show();
-    // Show 'about me' content.
-    $(".second").show();
-    // Show 'My Work' Content.
-    $(".third").show();
-    $(".aboutMeParagraph").show();
-    // Animate a smooth scroll.
-    $('html,body').animate({
-        scrollTop: $(".second").offset().top
-    }, 'slow'); // Do it slowly.
-
-    // Base Development Technologies (Progress Animation Transition).
-    $(".html").animate({
-        width: "100%"
-    }, 500);
-    $(".css").animate({
-        width: "100%"
-    }, 500);
-    $(".js").animate({
-        width: "85%"
-    }, 500);
-
-    // Base Tools, Frameworks and Libraries (Progress Animation Transition).
-    $(".vsCode").animate({
-        width: "90%"
-    }, 500);
-    $(".bsFramework").animate({
-        width: "85%"
-    }, 500);
-    $(".jQuery").animate({
-        width: "50%"
-    }, 500);
-});
-
-// Smooth Scroll to 'About Me'.
-$("#linkDiveDeeper").click(function () {
-    // Make the scroll wait a second so the IDS needed to load are present at the time of the scroll request.
-    setTimeout(100);
-    // Show 'about me' image.
-    $("#deepDiveBackground").show();
-    // Show 'about me' content.
-    $(".second").show();
-    // Show 'My Work' Content.
-    $(".third").show();
-    // Animate a smooth scroll.
-    $('html,body').animate({
-        scrollTop: $(".second").offset().top
-    }, 'slow'); // Do it slowly.
-
-    // Base Development Technologies (Progress Animation Transition).
-    $(".html").animate({
-        width: "100%"
-    }, 500);
-    $(".css").animate({
-        width: "100%"
-    }, 500);
-    $(".js").animate({
-        width: "85%"
-    }, 500);
-
-    // Base Tools, Frameworks and Libraries (Progress Animation Transition).
-    $(".vsCode").animate({
-        width: "90%"
-    }, 500);
-    $(".bsFramework").animate({
-        width: "85%"
-    }, 500);
-    $(".jQuery").animate({
-        width: "50%"
-    }, 500);
-});
-
-// Smooth Scroll to 'My Work'.
-$(".my-work").click(function () {
-    // Make the scroll wait a second so the IDS needed to load are present at the time of the scroll request.
-    setTimeout(100);
-    // Show 'about me' image.
-    $("#deepDiveBackground").show();
-    // Show 'about me' content.
-    $(".second").show();
-    // Show 'My Work' Content.
-    $(".third").show();
-    // Animate a smooth scroll.
-    $('html,body').animate({
-        scrollTop: $(".third").offset().top
-    }, 'slow'); // Do it slowly.
-
-    // Base Development Technologies (Progress Animation Transition).
-    $(".html").animate({
-        width: "100%"
-    }, 500);
-    $(".css").animate({
-        width: "100%"
-    }, 500);
-    $(".js").animate({
-        width: "85%"
-    }, 500);
-
-    // Base Tools, Frameworks and Libraries (Progress Animation Transition).
-    $(".vsCode").animate({
-        width: "90%"
-    }, 500);
-    $(".bsFramework").animate({
-        width: "85%"
-    }, 500);
-    $(".jQuery").animate({
-        width: "50%"
-    }, 500);
-});
-
-// Back to 'Home' Section.
-$(".home").click(function () {
-    // Make the scroll wait a second so the IDS needed to load are present at the time of the scroll request.
-    setTimeout(100);
-    // Animate a smooth scroll.
-    $('html,body').animate({
-        scrollTop: $(".first").offset().top
-    }, 'slow'); // Do it slowly.
-});
-
-// Prevent Project Slider From Auto Scrolling.
-$('.carousel').carousel({
-    interval: false
-});
-
-// Get footer year dynamically with moment.
-var currentYear = moment().format('YYYY');
-// Append current year to footer.
-document.getElementById("dynamicYear").textContent = currentYear;
