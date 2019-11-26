c742c46e7ecef7dfa9b5204161d862c50310ef6c
icepants <shawnluther99@gmail.com>
2019-11-26 16:41:25 -0500

    Fixed a security issue where href= did not include 'noopener noreferrer'.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -33,13 +33,13 @@ function Footer() {
                         <h5 class="text-uppercase">Social Media & News Articles</h5>
                         <ul class="list-unstyled">
                             <li>
-                                <u><a target="_blank" href="https://www.bizjournals.com/philadelphia/news/2017/03/27/global-tax-sap-pathway-school-hiring-autism-worker.html">Philadelphia Business Journal</a></u>
+                                <u><a target="_blank" href="https://www.bizjournals.com/philadelphia/news/2017/03/27/global-tax-sap-pathway-school-hiring-autism-worker.html" rel="noopener noreferrer">Philadelphia Business Journal</a></u>
                             </li>
                             <li>
-                                <u><a target="_blank" href="https://www.linkedin.com/in/shawn-luther/">LinkedIn</a></u>
+                                <u><a target="_blank" href="https://www.linkedin.com/in/shawn-luther/" rel="noopener noreferrer">LinkedIn</a></u>
                             </li>
                             <li>
-                                <u><a target="_blank" href="https://github.com/IAmShawn98">Github</a></u>
+                                <u><a target="_blank" href="https://github.com/IAmShawn98" rel="noopener noreferrer">Github</a></u>
                             </li>
                         </ul>
                     </div>
@@ -47,13 +47,13 @@ function Footer() {
                         <h5 class="text-uppercase">Shoutouts to These Awesome Resources</h5>
                         <ul class="list-unstyled">
                             <li>
-                                <u><a target="_blank" href="https://www.transparenttextures.com/">transparenttextures.com</a></u>
+                                <u><a target="_blank" href="https://www.transparenttextures.com/" rel="noopener noreferrer">transparenttextures.com</a></u>
                             </li>
                             <li>
-                                <u><a target="_blank" href="https://bennettfeely.com/clippy/">bennettfeely.com</a></u>
+                                <u><a target="_blank" href="https://bennettfeely.com/clippy/" rel="noopener noreferrer">bennettfeely.com</a></u>
                             </li>
                             <li>
-                                <u><a target="_blank" href="https://cssgradient.io/">cssgradient.io</a></u>
+                                <u><a target="_blank" href="https://cssgradient.io/" rel="noopener noreferrer">cssgradient.io</a></u>
                             </li>
                         </ul>
                     </div>
@@ -61,7 +61,7 @@ function Footer() {
             </div>
             {/* Author Credit */}
             <div class="footer-copyright text-center py-3"> A Thing By
-                <a href="https://www.linkedin.com/in/shawn-luther/"> Shawn Luther </a>
+                <a href="https://www.linkedin.com/in/shawn-luther/" rel="noopener noreferrer"> Shawn Luther </a>
                 2019
             </div>
         </footer>
